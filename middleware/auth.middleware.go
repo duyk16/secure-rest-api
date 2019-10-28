effba7ab2cdd3eb29a34ef08a7acabb329e47c77
Monkey <duyk16@gmail.com>
2019-10-28 20:54:39 +0700

    add middleware secure

---
+++ middleware/auth.middleware.go
@@ -0,0 +1,71 @@
+package middleware
+
+import (
+	"net/http"
+	"secure-rest-api/config"
+	u "secure-rest-api/util"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func JwtAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		noAuthRoutes := []string{
+			"/api/auth/login",
+			"/api/auth/register",
+		}
+
+		//check if request does not need authentication, serve the request if it doesn't need it
+		for _, value := range noAuthRoutes {
+			if value == r.URL.Path {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		tokenHeader := r.Header.Get("Authorization")
+		if tokenHeader == "" {
+			u.JSON(w, 400, u.T{
+				"status":  "error",
+				"message": "Missing token",
+			})
+			return
+		}
+
+		// `Bearer {token-body}`
+		splitted := strings.Split(tokenHeader, " ")
+		if len(splitted) != 2 {
+			u.JSON(w, 400, u.T{
+				"status":  "error",
+				"message": "Invalid/Malformed auth token",
+			})
+			return
+		}
+
+		tokenString := splitted[1]
+		token := u.Token{}
+
+		tokenData, err := jwt.ParseWithClaims(tokenString, &token, func(token *jwt.Token) (interface{}, error) {
+			return []byte(config.ServerConfig.JWTKey), nil
+		})
+
+		if err != nil {
+			u.JSON(w, http.StatusForbidden, u.T{
+				"status":  "error",
+				"message": "Malformed authentication token",
+			})
+			return
+		}
+
+		if !tokenData.Valid {
+			u.JSON(w, http.StatusForbidden, u.T{
+				"status":  "error",
+				"message": "Token is not valid.",
+			})
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
--- router/router.go
+++ router/router.go
@@ -3,7 +3,9 @@ package router
 import (
 	"log"
 	"net/http"
+
 	"secure-rest-api/controller"
+	"secure-rest-api/middleware"
 
 	"github.com/gorilla/mux"
 )
@@ -12,9 +14,12 @@ func Init() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/api/auth/login", controller.PostLogin).Methods("POST")
-	r.HandleFunc("/api/user", controller.CreateUser).Methods("POST")
+	r.HandleFunc("/api/auth/register", controller.CreateUser).Methods("POST")
 
 	r.HandleFunc("/api/user/{userId}", controller.GetUserById).Methods("GET")
+
+	// Secure api with authentication middleware
+	r.Use(middleware.JwtAuthentication)
 
 	listen(r, "8000")
 }
